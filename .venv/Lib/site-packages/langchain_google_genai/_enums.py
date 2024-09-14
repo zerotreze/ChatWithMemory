f81878380de8eacbfacb5f958493adf71bbe79f1
José Vitor <blackdarkfur@hotmail.com>
2024-09-14 18:17:17 -0300

    .

--- .venv/Lib/site-packages/langchain_google_genai/_enums.py
+++
@@ -1,6 +0,0 @@
-import google.ai.generativelanguage_v1beta as genai
-
-HarmBlockThreshold = genai.SafetySetting.HarmBlockThreshold
-HarmCategory = genai.HarmCategory
-
-__all__ = ["HarmBlockThreshold", "HarmCategory"]
